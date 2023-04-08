8791227e1e63242631dd62eb175359883c893cae
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2023-04-08 11:39:33 -0500

    Add files via upload

--- constants.h
+++ constants.h
@@ -1,7 +1,8 @@
-//FILE: Constants.h
 #define WS 1
 #define WS_UP 1200
 #define WS_DOWN 150
+
+#define LIGHT 1
 
 #define HOOK 0
 #define HOOK_UP 0
@@ -11,4 +12,6 @@
 #define LEFT_LINE 0
 #define RIGHT_LINE 5
 
-#define BLACK 3000
+#define CLICK_SENSOR 0
+
+#define BLACK 2000
